019556b6f97b0c197ecb8c18bf517e72a2377dd9
Daniel Meszaros <easimer@gmail.com>
2021-03-18 22:25:48 +0100

    Initial implementation of the turtle VM

--- space/components/turtle-gfx/index.js
+++ space/components/turtle-gfx/index.js
@@ -1,48 +1,192 @@
 const { wom } = require('maxwhere');
-const { ipcMain } = require('electron');
+const { ipcMain  } = require('electron');
 const path = require('path');
 const WebSocket = require('ws');
 const log = require('electron-log');
 
-var wss = null;
+const TURTLE_INIT_POSITION = { x: 0, y: 0, z: 0 };
+const TURTLE_INIT_ORIENTATION = { w: -1, x: 0, y: 0, z: 0 };
+const TURTLE_INIT_SCALE = 1.0
+
+let wsServer = null;
+let hTurtle = null
+
+function resetTurtle() {
+  hTurtle.setPosition(TURTLE_INIT_POSITION, 'absolute', 'world');
+  hTurtle.setOrientation(TURTLE_INIT_ORIENTATION);
+}
+
+function eulerToQuaternion(yaw, pitch, roll) {
+  let cy = Math.cos(yaw * 0.5);
+  let sy = Math.sin(yaw * 0.5);
+  let cp = Math.cos(pitch * 0.5);
+  let sp = Math.sin(pitch * 0.5);
+  let cr = Math.cos(roll * 0.5);
+  let sr = Math.sin(roll * 0.5);
+
+  let w = cr * cp * cy + sr * sp * sy;
+  let x = sr * cp * cy - cr * sp * sy;
+  let y = cr * sp * cy + sr * cp * sy;
+  let z = cr * cp * sy - sr * sp * cy;
+
+  return {w: w, x: x, y: y, z: z};
+}
+
+function getTurtleDirectionVector(state) {
+  let dx = Math.cos(state.yaw) * Math.cos(state.pitch);
+  let dy = Math.sin(state.yaw) * Math.cos(state.pitch);
+  let dz = Math.sin(state.pitch);
+  return { x: dx, y: dy, z: dz };
+}
+
+function setStatePosition(state, vec) {
+  state.x = vec.x;
+  state.y = vec.y;
+  state.z = vec.z;
+}
+
+function getStatePosition(state) {
+  return { x: state.x, y: state.y, z: state.z };
+}
+
+function updateTurtleObject(state) {
+  hTurtle.setOrientation(eulerToQuaternion(state.yaw, state.pitch, state.roll));
+  hTurtle.setPosition(getStatePosition(state));
+}
+
+function vecScaleAdd(lhs, scale, rhs) {
+  return {
+    x: lhs.x + scale * rhs.x,
+    y: lhs.y + scale * rhs.y,
+    z: lhs.z + scale * rhs.z,
+  };
+}
+
+const vmDispatchTable = {
+  'TOP' : (state, instruction) => {
+    log.debug("processing children");
+    for(const child of instruction.children) {
+      log.debug("processing child " + child.id);
+      decodeInstruction(state, child);
+    }
+  },
+  'MOVE_FORWARD' : (state, instruction) => {
+    const distance = parseInt(instruction.arg);
+    let dir = getTurtleDirectionVector(state.state);
+    let newPos = vecScaleAdd(getStatePosition(state.state), distance, dir);
+    setStatePosition(state.state, newPos);
+    updateTurtleObject(state.state);
+  },
+
+  "MOVE_BACKWARD" : (state, instruction) => {
+    const distance = parseInt(instruction.arg);
+    let dir = getTurtleDirectionVector(state.state);
+    let newPos = vecScaleAdd(getStatePosition(state.state), -distance, dir);
+    setStatePosition(state.state, newPos);
+    updateTurtleObject(state.state);
+  },
+
+  "ROTATE_YAW" : (state, instruction) => {
+  },
+
+  "ROTATE_PITCH" : (state, instruction) => {
+  },
+
+  "ROTATE_ROLL" : (state, instruction) => {
+  },
+
+  "REPEAT" : (state, instruction) => {
+    const times = parseInt(instruction.arg);
+    for(var i = 0; i < times; i++) {
+      for(const child of instruction.children) {
+        decodeInstruction(state, child);
+      }
+    }
+  },
+
+  "STATE_PUSH" : (state, instruction) => {
+    state.stack.push(Object.assign({}, state.state));
+  },
+
+  "STATE_POP" : (state, instruction) => {
+    state.state = Object.assign({}, state.stack.pop());
+    updateTurtleObject(state.state);
+  },
+
+  "PEN_DOWN" : (state, instruction) => {
+  },
+
+  "PEN_UP" : (state, instruction) => {
+  },
+
+  "PEN_COLOR" : (state, instruction) => {
+  },
+};
+
+function decodeInstruction(state, instruction) {
+  log.debug("decodeInstruction: " + instruction.id + " " + instruction.arg);
+
+  vmDispatchTable[instruction.id](state, instruction);
+}
+
+function executeProgram(program) {
+  var state = {
+    stack: [],
+    state: {
+      x: 0, y: 0, z: 0,
+      yaw: 0, pitch: 0, roll: 0,
+      pen_active: true,
+      r: 255, g: 0, b: 0,
+    }
+  };
+  resetTurtle();
+  // Start at TOP block
+  decodeInstruction(state, program);
+}
 
 function onMessageReceived(message) {
-  log.info("message: '" + message + "'");
-
   const msg = JSON.parse(message);
   if(msg != null) {
-    log.info("Message received:");
-    log.info(msg);
+    log.debug("Message received:");
+    log.debug(msg);
+
+    switch(msg.type) {
+      case 'execProgram':
+        executeProgram(msg.program);
+        break;
+    }
+  } else {
+    log.error("message received didn't parse!");
   }
 }
 
 const init = (props) => {
-  log.info(props);
-  wss = new WebSocket.Server({ port: 8080 });
-  if(wss) {
-    log.info('WS server running');
+  wsServer = new WebSocket.Server({ port: 8080 });
+
+  if(wsServer) {
+    log.debug('WS server running');
   } else {
-    log.info('WS server didn\'t start');
+    log.debug('WS server didn\'t start');
   }
 
-  if(wss != null) {
-    wss.on('connection', function connection(ws) {
-      log.info("new connection: " + ws);
-      ws.on('message', onMessageReceived);
+  if(wsServer != null) {
+    wsServer.on('connection', function connection(pClient) {
+      log.debug("new connection: " + pClient);
+      pClient.on('message', onMessageReceived);
     });
   }
 
-  log.info('ready');
+  log.debug('ready');
 };
 
 const done = () => {
-  log.info('done');
+  log.debug('done');
 };
 
 const IPC_CHANNEL_DEBUG = 'turtle-debug';
 
 const render = (props, children) => {
-  let debug_panel = wom.create('billboard', {
+  let debugPanel = wom.create('billboard', {
     url: path.resolve(__dirname, 'index.html'),
     nodeIntegration: true, // need this for IPC communication
     width: 586,
@@ -53,20 +197,24 @@ const render = (props, children) => {
     orientation: { "x": 0, "y": 0, "z": 0, "w": -1 },
     physical: {raycast: true} // accept mouse events to make it orbitable
   });
-  wom.render(debug_panel);
+  wom.render(debugPanel);
+
+  hTurtle = wom.create('mesh', {
+    id: 'turtle',
+    url: 'penguin.mesh',
+    position: TURTLE_INIT_POSITION,
+    orientation: TURTLE_INIT_ORIENTATION,
+    scale: TURTLE_INIT_SCALE,
+    autophysical: true
+  });
+  wom.render(hTurtle);
   
-  // using ipc listeners
-  // IMPORTANT: callback registrations are not idempotent!
-  // if you run a space N times and register the same callback N times
-  // the callback will be executed N times. So, first we remove all listeners
-  // for the given channel:
   ipcMain.removeAllListeners(IPC_CHANNEL_DEBUG);
   ipcMain.on(IPC_CHANNEL_DEBUG, (event, payload) => {
-    log.info('IPC: ', payload);
+    log.debug('IPC: ', payload);
     if (payload == "kill-ws") {
       if(ws != null) {
         ws.close();
-        ws = null;
       }
     }
   });
