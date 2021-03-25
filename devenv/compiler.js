a8fea04b2b9aa7a18781610f01e889450dc6e097
Daniel Meszaros <easimer@gmail.com>
2021-03-25 15:18:54 +0100

    devenv: fix code style

--- devenv/compiler.js
+++ devenv/compiler.js
@@ -1,74 +1,74 @@
 function getCommandKind(node) {
-    return node.getAttribute("commandkind");
+  return node.getAttribute('commandkind');
 }
 
 function getCommandArgument(node) {
-    let elemArgument = node.querySelector(".argument");
-    if(elemArgument != null) {
-        return elemArgument.value;
-    } else {
-        return null;
-    }
+  let elemArgument = node.querySelector('.argument');
+  if(elemArgument != null) {
+    return elemArgument.value;
+  } else {
+    return null;
+  }
 }
 
 const parserNodeConverter = {
-    makeEmpty : function() {
-        return { id: "TOP", arg: "", children: [], node: null };
-    },
-    convert : function(root) {
-        return { id: getCommandKind(root), arg: getCommandArgument(root), children: [], node: root };
-    },
-    appendResult : function(accumulator, result) {
-        accumulator.children.push(result)
-        return accumulator
-    },
+  makeEmpty : function() {
+    return { id: 'TOP', arg: '', children: [], node: null };
+  },
+  convert : function(root) {
+    return { id: getCommandKind(root), arg: getCommandArgument(root), children: [], node: root };
+  },
+  appendResult : function(accumulator, result) {
+    accumulator.children.push(result);
+    return accumulator;
+  },
 };
 
 function processSubcommands(ret, elemSubcommands, nodeConverter) {
-    let children = elemSubcommands.childNodes;
+  let children = elemSubcommands.childNodes;
 
-    for(let i = 0; i < children.length; i++) {
-        let child = children[i];
-        if(child.classList === undefined) {
-            continue;
-        }
-        if(child.classList.contains("baseCommand")) {
-            let result = traverseHtmlTree(child, nodeConverter);
-            ret = nodeConverter.appendResult(ret, result)
-        }
+  for(let i = 0; i < children.length; i++) {
+    let child = children[i];
+    if(child.classList === undefined) {
+      continue;
     }
+    if(child.classList.contains('baseCommand')) {
+      let result = traverseHtmlTree(child, nodeConverter);
+      ret = nodeConverter.appendResult(ret, result);
+    }
+  }
 
-    return ret;
+  return ret;
 }
 
 function traverseHtmlTree(tree, nodeConverter) {
-    var ret = nodeConverter.convert(tree)
+  let ret = nodeConverter.convert(tree);
 
-    let elemSubcommands = tree.querySelector(".subcommands");
-    ret = processSubcommands(ret, elemSubcommands, nodeConverter);
+  let elemSubcommands = tree.querySelector('.subcommands');
+  ret = processSubcommands(ret, elemSubcommands, nodeConverter);
 
-    return ret;
+  return ret;
 }
 
-function makeProgramAST(treeRoot) {
-    var ret = parserNodeConverter.makeEmpty();
-    ret = processSubcommands(ret, treeRoot, parserNodeConverter);
-    return ret;
+export function makeProgramAST(treeRoot) {
+  let ret = parserNodeConverter.makeEmpty();
+  ret = processSubcommands(ret, treeRoot, parserNodeConverter);
+  return ret;
 }
 
-function dumpAST(ast) {
-    var ret = "(" + ast.id + ", " + ast.arg + ")";
+export function dumpAST(ast) {
+  let ret = '(' + ast.id + ', ' + ast.arg + ')';
 
-    if(ast.children.length > 0) {
-        ret += "[";
-        for(let i = 0; i < ast.children.length; i++) {
-            ret += dumpAST(ast.children[i]);
-            if(i != ast.children.length - 1) {
-                ret += ", ";
-            }
-        }
-        ret += "]";
+  if(ast.children.length > 0) {
+    ret += '[';
+    for(let i = 0; i < ast.children.length; i++) {
+      ret += dumpAST(ast.children[i]);
+      if(i != ast.children.length - 1) {
+        ret += ', ';
+      }
     }
+    ret += ']';
+  }
 
-    return ret;
+  return ret;
 }
