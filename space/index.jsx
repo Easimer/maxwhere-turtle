a3e763cd219fb45768f8fb6688104ff34b286ab3
Daniel Meszaros <easimer@gmail.com>
2021-04-23 21:15:01 +0200

    Add a background

--- space/index.jsx
+++ space/index.jsx
@@ -1,22 +1,373 @@
 <root>
-<turtle-gfx/>
-
-<node
-  position={{ x: 0, y: 100, z: 0 }}
-  orientation={{ x: 0, y: 0, z: 0, w: 1 }}>
-  <light {...{ lighttype: 'directional'}}
-    direction={{ x: 1, y: 1, z: 1 }}
-    diffuse={{ r: 0.800000011920929, g: 0.800000011920929, b: 0.800000011920929 }}
-    specular={{ r: 0.800000011920929, g: 0.800000011920929, b: 0.800000011920929}}>
-  </light>
-</node>
-<node
-  position={{ x: 0, y: 0, z: 0 }}
-  orientation={{ x: 0, y: 0, z: 0, w: 1 }}>
-  <light {...{ lighttype: 'directional'}}
-    direction={{ x: -1, y: -1, z: -1 }}
-    diffuse={{ r: 0.800000011920929, g: 0.800000011920929, b: 0.800000011920929 }}
-    specular={{ r: 0.800000011920929, g: 0.800000011920929, b: 0.800000011920929}}>
-  </light>
-</node>
+  <turtle-gfx/>
+  <node
+    position={{ x: -3842.751953125, y: 0.0, z: 7122.3154296875 }}
+    orientation={{ w: 1.0, x: 0.0, y: 0.0, z: -0.0 }}
+    scale={{ x: 1.0, y: 1.0, z: 1.0 }}>
+    <mesh
+      {...{ url: 'Canyon.Wall.004.mesh' }}
+      physical={{ raycast: true }}
+      done={ m => {m.addPhysicalShape({shape: 'mesh', mesh: 'Canyon.Wall.004_phy.obj', mass: 1 })  } }>
+    </mesh>
+  </node>
+  <node
+    position={{ x: 814.93017578125, y: 0.0, z: 7122.3154296875 }}
+    orientation={{ w: 7.549790126404332e-08, x: 0.0, y: 1.0, z: -0.0 }}
+    scale={{ x: 1.0, y: 1.0, z: 1.0 }}>
+    <mesh
+      {...{ url: 'Canyon.Wall.005.mesh' }}
+      physical={{ raycast: true }}
+      done={ m => {m.addPhysicalShape({shape: 'mesh', mesh: 'Canyon.Wall.005_phy.obj', mass: 1 })  } }>
+    </mesh>
+  </node>
+  <node
+    position={{ x: 262.8330078125, y: 0.0, z: 9542.154296875 }}
+    orientation={{ w: 0.2164396345615387, x: 0.0, y: 0.9762960076332092, z: -0.0 }}
+    scale={{ x: 1.0, y: 1.0, z: 1.0 }}>
+    <mesh
+      {...{ url: 'Canyon.Wall.001.mesh' }}
+      physical={{ raycast: true }}
+      done={ m => {m.addPhysicalShape({shape: 'mesh', mesh: 'Canyon.Wall.001_phy.obj', mass: 1 })  } }>
+    </mesh>
+  </node>
+  <node
+    position={{ x: -1354.450927734375, y: 0.0, z: 11272.271484375 }}
+    orientation={{ w: 0.4999999403953552, x: 0.0, y: 0.8660253882408142, z: -0.0 }}
+    scale={{ x: 1.0, y: 1.0, z: 1.0 }}>
+    <mesh
+      {...{ url: 'Canyon.Wall.001.mesh' }}
+      physical={{ raycast: true }}
+      done={ m => {m.addPhysicalShape({shape: 'mesh', mesh: 'Canyon.Wall.001_phy.obj', mass: 1 })  } }>
+    </mesh>
+  </node>
+  <node
+    position={{ x: -4469.60693359375, y: 0.0, z: 9554.509765625 }}
+    orientation={{ w: 0.9724107384681702, x: 0.0, y: -0.23327524960041046, z: -0.0 }}
+    scale={{ x: 1.0, y: 1.0, z: 1.0 }}>
+    <mesh
+      {...{ url: 'Canyon.Wall.001.mesh' }}
+      physical={{ raycast: true }}
+      done={ m => {m.addPhysicalShape({shape: 'mesh', mesh: 'Canyon.Wall.001_phy.obj', mass: 1 })  } }>
+    </mesh>
+  </node>
+  <node
+    position={{ x: 898.5107421875, y: 0.0, z: -3870.49267578125 }}
+    orientation={{ w: 7.549790126404332e-08, x: 0.0, y: 1.0, z: -0.0 }}
+    scale={{ x: 1.0, y: 1.0, z: 1.0 }}>
+    <mesh
+      {...{ url: 'Canyon.Wall.003.mesh' }}
+      physical={{ raycast: true }}
+      done={ m => {m.addPhysicalShape({shape: 'mesh', mesh: 'Canyon.Wall.003_phy.obj', mass: 1 })  } }>
+    </mesh>
+  </node>
+  <node
+    position={{ x: 1724.1304931640625, y: 0.0, z: 2200.9541015625 }}
+    orientation={{ w: 0.4921141266822815, x: 0.0, y: 0.8705307245254517, z: -0.0 }}
+    scale={{ x: 0.9999999403953552, y: 0.9999999403953552, z: 1.0 }}>
+    <mesh
+      {...{ url: 'Canyon.Wall.001.mesh' }}
+      physical={{ raycast: true }}
+      done={ m => {m.addPhysicalShape({shape: 'mesh', mesh: 'Canyon.Wall.001_phy.obj', mass: 1 })  } }>
+    </mesh>
+  </node>
+  <node
+    position={{ x: 2030.562255859375, y: 0.0, z: -2120.1689453125 }}
+    orientation={{ w: 0.6154888272285461, x: 0.0, y: -0.7881456017494202, z: -0.0 }}
+    scale={{ x: 0.9999999403953552, y: 0.9999999403953552, z: 1.0 }}>
+    <mesh
+      {...{ url: 'Canyon.Wall.001.mesh' }}
+      physical={{ raycast: true }}
+      done={ m => {m.addPhysicalShape({shape: 'mesh', mesh: 'Canyon.Wall.001_phy.obj', mass: 1 })  } }>
+    </mesh>
+  </node>
+  <node
+    position={{ x: -3672.508544921875, y: 0.0, z: 12117.1708984375 }}
+    orientation={{ w: 0.638795018196106, x: 0.0, y: 0.7693769931793213, z: -0.0 }}
+    scale={{ x: 1.0, y: 1.0, z: 1.0 }}>
+    <mesh
+      {...{ url: 'Canyon.Wall.001.mesh' }}
+      physical={{ raycast: true }}
+      done={ m => {m.addPhysicalShape({shape: 'mesh', mesh: 'Canyon.Wall.001_phy.obj', mass: 1 })  } }>
+    </mesh>
+  </node>
+  <node
+    position={{ x: -6202.7333984375, y: 0.0, z: 10962.67578125 }}
+    orientation={{ w: 0.8000754714012146, x: 0.0, y: -0.5998995304107666, z: -0.0 }}
+    scale={{ x: 1.0, y: 1.0, z: 1.0 }}>
+    <mesh
+      {...{ url: 'Canyon.Wall.001.mesh' }}
+      physical={{ raycast: true }}
+      done={ m => {m.addPhysicalShape({shape: 'mesh', mesh: 'Canyon.Wall.001_phy.obj', mass: 1 })  } }>
+    </mesh>
+  </node>
+  <node
+    position={{ x: -6272.19921875, y: 0.0, z: 12528.7880859375 }}
+    orientation={{ w: 0.6763516664505005, x: 0.0, y: 0.7365788817405701, z: -0.0 }}
+    scale={{ x: 1.0, y: 1.0, z: 1.0 }}>
+    <mesh
+      {...{ url: 'Canyon.Wall.001.mesh' }}
+      physical={{ raycast: true }}
+      done={ m => {m.addPhysicalShape({shape: 'mesh', mesh: 'Canyon.Wall.001_phy.obj', mass: 1 })  } }>
+    </mesh>
+  </node>
+  <node
+    position={{ x: -2697.5224609375, y: 4210.18896484375, z: -0.0 }}
+    orientation={{ w: 0.9956948161125183, x: 0.0, y: 0.0, z: 0.09269235283136368 }}>
+    <light
+      {...{ lighttype: 'directional' }}
+      direction={{ x: 0, y: -1, z: 0 }}
+      diffuse={{ r: 1.0, g: 0.8722190260887146, b: 0.8187828063964844 }}
+      specular={{ r: 1.0, g: 0.8722190260887146, b: 0.8187828063964844 }}>
+    </light>
+  </node>
+  <node
+    position={{ x: 3419.385498046875, y: 0.0, z: -0.0 }}
+    orientation={{ w: 1.0, x: 0.0, y: 0.0, z: -0.0 }}
+    scale={{ x: 1.0, y: 1.0, z: 1.0 }}>
+    <mesh
+      {...{ url: 'Petra.001.mesh' }}
+      physical={{ raycast: true }}
+      done={ m => {m.addPhysicalShape({shape: 'mesh', mesh: 'Petra.001_phy.obj', mass: 1 })  } }>
+    </mesh>
+  </node>
+  <node
+    position={{ x: 3567.85498046875, y: 0.0, z: -88.18421173095703 }}
+    orientation={{ w: 0.00908211711794138, x: -0.0, y: -0.9999587535858154, z: 0.0 }}
+    scale={{ x: 0.9999999403953552, y: 0.9999999403953552, z: 1.0 }}>
+    <mesh
+      {...{ url: 'Canyon.Wall.002.mesh' }}
+      physical={{ raycast: true }}
+      done={ m => {m.addPhysicalShape({shape: 'mesh', mesh: 'Canyon.Wall.002_phy.obj', mass: 1 })  } }>
+    </mesh>
+  </node>
+  <node
+    position={{ x: 3755.28173828125, y: 0.0, z: 1105.5091552734375 }}
+    orientation={{ w: 0.4921141266822815, x: 0.0, y: 0.8705307245254517, z: -0.0 }}
+    scale={{ x: 0.9999999403953552, y: 0.9999999403953552, z: 1.0 }}>
+    <mesh
+      {...{ url: 'Canyon.Wall.001.mesh' }}
+      physical={{ raycast: true }}
+      done={ m => {m.addPhysicalShape({shape: 'mesh', mesh: 'Canyon.Wall.001_phy.obj', mass: 1 })  } }>
+    </mesh>
+  </node>
+  <node
+    position={{ x: 4241.38427734375, y: 0.0, z: 149.2502899169922 }}
+    orientation={{ w: 0.00908211711794138, x: -0.0, y: -0.9999587535858154, z: 0.0 }}
+    scale={{ x: 0.9999999403953552, y: 0.9999999403953552, z: 1.0 }}>
+    <mesh
+      {...{ url: 'Canyon.Wall.001.mesh' }}
+      physical={{ raycast: true }}
+      done={ m => {m.addPhysicalShape({shape: 'mesh', mesh: 'Canyon.Wall.001_phy.obj', mass: 1 })  } }>
+    </mesh>
+  </node>
+  <node
+    position={{ x: 3316.79736328125, y: 0.0, z: -1417.532958984375 }}
+    orientation={{ w: 0.5431917905807495, x: 0.0, y: -0.8396086096763611, z: -0.0 }}
+    scale={{ x: 0.9999999403953552, y: 0.9999999403953552, z: 1.0 }}>
+    <mesh
+      {...{ url: 'Canyon.Wall.001.mesh' }}
+      physical={{ raycast: true }}
+      done={ m => {m.addPhysicalShape({shape: 'mesh', mesh: 'Canyon.Wall.001_phy.obj', mass: 1 })  } }>
+    </mesh>
+  </node>
+  <node
+    position={{ x: -1595.7498779296875, y: 300.0, z: -8022.3740234375 }}
+    orientation={{ w: 1.0, x: 0.0, y: 0.0, z: -0.0 }}
+    scale={{ x: 1.0, y: 1.0, z: 1.0 }}>
+    <mesh
+      {...{ url: 'Canyon.Slot.001.mesh' }}
+      physical={{ raycast: true }}
+      done={ m => {m.addPhysicalShape({shape: 'mesh', mesh: 'Canyon.Slot.001_phy.obj', mass: 1 })  } }>
+    </mesh>
+  </node>
+  <node
+    position={{ x: -4000.0, y: 0.0, z: 12000.0 }}
+    orientation={{ w: 1.0, x: 0.0, y: 0.0, z: -0.0 }}
+    scale={{ x: 1.0, y: 1.0, z: 1.0 }}>
+    <mesh
+      {...{ url: 'Ground.001.mesh' }}
+      physical={{ raycast: true }}
+      done={ m => {m.addPhysicalShape({shape: 'mesh', mesh: 'Ground.001_phy.obj', mass: 1 })  } }>
+    </mesh>
+  </node>
+  <node
+    position={{ x: 2998.2080078125, y: 0.0, z: -0.0 }}
+    orientation={{ w: 1.0, x: 0.0, y: 0.0, z: -0.0 }}
+    scale={{ x: 1.0, y: 1.0, z: 1.0 }}>
+    <mesh
+      {...{ url: 'Ground.Petra.mesh' }}
+      physical={{ raycast: true }}
+      done={ m => {m.addPhysicalShape({shape: 'mesh', mesh: 'Ground.Petra_phy.obj', mass: 1 })  } }>
+    </mesh>
+  </node>
+  <node
+    position={{ x: -1400.0, y: 0.0, z: -7000.0 }}
+    orientation={{ w: 1.0, x: 0.0, y: 0.0, z: -0.0 }}
+    scale={{ x: 1.0, y: 1.0, z: 1.0 }}>
+    <mesh
+      {...{ url: 'Ground.Slot.mesh' }}
+      physical={{ raycast: true }}
+      done={ m => {m.addPhysicalShape({shape: 'mesh', mesh: 'Ground.Slot_phy.obj', mass: 1 })  } }>
+    </mesh>
+  </node>
+  <node
+    position={{ x: 553.2254028320312, y: 89.37971496582031, z: 3103.309814453125 }}
+    orientation={{ w: 1.0, x: 0.0, y: 0.0, z: -0.0 }}
+    scale={{ x: 1.0, y: 1.0, z: 1.0 }}>
+    <mesh
+      {...{ url: 'Canyon.Rock.001.mesh' }}
+      physical={{ raycast: true }}
+      done={ m => {m.addPhysicalShape({shape: 'mesh', mesh: 'Canyon.Rock.001_phy.obj', mass: 1 })  } }>
+    </mesh>
+  </node>
+  <node
+    position={{ x: 635.7313842773438, y: 0.13885498046875, z: 2067.911376953125 }}
+    orientation={{ w: 1.0, x: 0.0, y: 0.0, z: -0.0 }}
+    scale={{ x: 1.0, y: 1.0, z: 1.0 }}>
+    <mesh
+      {...{ url: 'Canyon.Cactus.001.mesh' }}
+      physical={{ raycast: true }}
+      done={ m => {m.addPhysicalShape({shape: 'mesh', mesh: 'Canyon.Cactus.001_phy.obj', mass: 1 })  } }>
+    </mesh>
+  </node>
+  <node
+    position={{ x: 355.6092224121094, y: 0.13885498046875, z: -2892.918701171875 }}
+    orientation={{ w: 0.9411060214042664, x: 0.0, y: -0.33811190724372864, z: -0.0 }}
+    scale={{ x: 1.0, y: 1.0, z: 1.0 }}>
+    <mesh
+      {...{ url: 'Canyon.Cactus.001.mesh' }}
+      physical={{ raycast: true }}
+      done={ m => {m.addPhysicalShape({shape: 'mesh', mesh: 'Canyon.Cactus.001_phy.obj', mass: 1 })  } }>
+    </mesh>
+  </node>
+  <node
+    position={{ x: -3126.203125, y: 0.13885498046875, z: -5553.4970703125 }}
+    orientation={{ w: 0.9859081506729126, x: 0.0, y: 0.16728755831718445, z: -0.0 }}
+    scale={{ x: 1.0, y: 1.0, z: 1.0 }}>
+    <mesh
+      {...{ url: 'Canyon.Cactus.001.mesh' }}
+      physical={{ raycast: true }}
+      done={ m => {m.addPhysicalShape({shape: 'mesh', mesh: 'Canyon.Cactus.001_phy.obj', mass: 1 })  } }>
+    </mesh>
+  </node>
+  <node
+    position={{ x: -3274.19482421875, y: 0.13885498046875, z: -5258.60546875 }}
+    orientation={{ w: 0.47438961267471313, x: -0.0, y: -0.8803150057792664, z: 0.0 }}
+    scale={{ x: 1.0, y: 1.0, z: 1.0 }}>
+    <mesh
+      {...{ url: 'Canyon.Cactus.001.mesh' }}
+      physical={{ raycast: true }}
+      done={ m => {m.addPhysicalShape({shape: 'mesh', mesh: 'Canyon.Cactus.001_phy.obj', mass: 1 })  } }>
+    </mesh>
+  </node>
+  <node
+    position={{ x: 194.69969177246094, y: 0.13885498046875, z: 8307.0361328125 }}
+    orientation={{ w: 0.9681307673454285, x: 0.0, y: -0.25044527649879456, z: -0.0 }}
+    scale={{ x: 1.0, y: 1.0, z: 1.0 }}>
+    <mesh
+      {...{ url: 'Canyon.Cactus.001.mesh' }}
+      physical={{ raycast: true }}
+      done={ m => {m.addPhysicalShape({shape: 'mesh', mesh: 'Canyon.Cactus.001_phy.obj', mass: 1 })  } }>
+    </mesh>
+  </node>
+  <node
+    position={{ x: 350.5216064453125, y: -0.1861572265625, z: -3650.666748046875 }}
+    orientation={{ w: 0.9857801795005798, x: 0.0, y: 0.16804000735282898, z: -0.0 }}
+    scale={{ x: 1.0, y: 1.0, z: 1.0 }}>
+    <mesh
+      {...{ url: 'Canyon.Rock.002.mesh' }}
+      physical={{ raycast: true }}
+      done={ m => {m.addPhysicalShape({shape: 'mesh', mesh: 'Canyon.Rock.002_phy.obj', mass: 1 })  } }>
+    </mesh>
+  </node>
+  <node
+    position={{ x: -3369.11572265625, y: 11.03997802734375, z: -5436.904296875 }}
+    orientation={{ w: 0.6340948939323425, x: -0.701310932636261, y: -0.21844249963760376, z: 0.24159803986549377 }}
+    scale={{ x: 1.0, y: 1.0000001192092896, z: 1.0000001192092896 }}>
+    <mesh
+      {...{ url: 'Canyon.Rock.002.mesh' }}
+      physical={{ raycast: true }}
+      done={ m => {m.addPhysicalShape({shape: 'mesh', mesh: 'Canyon.Rock.002_phy.obj', mass: 1 })  } }>
+    </mesh>
+  </node>
+  <node
+    position={{ x: 1056.2435302734375, y: -0.1861572265625, z: -2052.701171875 }}
+    orientation={{ w: 0.4719560146331787, x: -0.13820670545101166, y: 0.8674690127372742, z: 0.0751928761601448 }}
+    scale={{ x: 1.000000238418579, y: 1.000000238418579, z: 1.0 }}>
+    <mesh
+      {...{ url: 'Canyon.Rock.002.mesh' }}
+      physical={{ raycast: true }}
+      done={ m => {m.addPhysicalShape({shape: 'mesh', mesh: 'Canyon.Rock.002_phy.obj', mass: 1 })  } }>
+    </mesh>
+  </node>
+  <node
+    position={{ x: -3701.26025390625, y: 89.37971496582031, z: -271.872314453125 }}
+    orientation={{ w: 0.7965395450592041, x: -0.09251712262630463, y: 0.5839874744415283, z: -0.12619028985500336 }}
+    scale={{ x: 1.0, y: 1.0, z: 1.0 }}>
+    <mesh
+      {...{ url: 'Canyon.Rock.001.mesh' }}
+      physical={{ raycast: true }}
+      done={ m => {m.addPhysicalShape({shape: 'mesh', mesh: 'Canyon.Rock.001_phy.obj', mass: 1 })  } }>
+    </mesh>
+  </node>
+  <node
+    position={{ x: -3696.363037109375, y: 89.37971496582031, z: 9205.7265625 }}
+    orientation={{ w: 0.9535924196243286, x: 0.025981850922107697, y: -0.24920427799224854, z: 0.1669841706752777 }}
+    scale={{ x: 1.0, y: 1.0, z: 1.0 }}>
+    <mesh
+      {...{ url: 'Canyon.Rock.001.mesh' }}
+      physical={{ raycast: true }}
+      done={ m => {m.addPhysicalShape({shape: 'mesh', mesh: 'Canyon.Rock.001_phy.obj', mass: 1 })  } }>
+    </mesh>
+  </node>
+  <node
+    position={{ x: 491.6253662109375, y: 1.832747459411621, z: 4874.32177734375 }}
+    orientation={{ w: 0.26081421971321106, x: 0.04754404351115227, y: -0.9524814486503601, z: -0.14998207986354828 }}
+    scale={{ x: 1.0, y: 1.0, z: 1.0 }}>
+    <mesh
+      {...{ url: 'Canyon.Rock.002.mesh' }}
+      physical={{ raycast: true }}
+      done={ m => {m.addPhysicalShape({shape: 'mesh', mesh: 'Canyon.Rock.002_phy.obj', mass: 1 })  } }>
+    </mesh>
+  </node>
+  <node
+    position={{ x: 422.5673828125, y: 3.655242919921875, z: 4576.77392578125 }}
+    orientation={{ w: 1.0, x: 0.0, y: 0.0, z: -0.0 }}
+    scale={{ x: 1.0, y: 1.0, z: 1.0 }}>
+    <mesh
+      {...{ url: 'Canyon.Tree.001.mesh' }}
+      physical={{ raycast: true }}
+      done={ m => {m.addPhysicalShape({shape: 'mesh', mesh: 'Canyon.Tree.001_phy.obj', mass: 1 })  } }>
+    </mesh>
+  </node>
+  <node
+    position={{ x: 1004.3711547851562, y: -35.97727966308594, z: 1860.3460693359375 }}
+    orientation={{ w: 0.8703069090843201, x: 0.05085499584674835, y: -0.28431475162506104, z: -0.39892950654029846 }}
+    scale={{ x: 1.0, y: 1.0, z: 1.0 }}>
+    <mesh
+      {...{ url: 'Bones.001.mesh' }}
+      physical={{ raycast: true }}
+      done={ m => {m.addPhysicalShape({shape: 'mesh', mesh: 'Bones.001_phy.obj', mass: 1 })  } }>
+    </mesh>
+  </node>
+  <node
+    position={{ x: 1124.29833984375, y: -60.825218200683594, z: 1700.30078125 }}
+    orientation={{ w: 0.26081421971321106, x: 0.04754404351115227, y: -0.9524814486503601, z: -0.14998207986354828 }}
+    scale={{ x: 1.0, y: 1.0, z: 1.0 }}>
+    <mesh
+      {...{ url: 'Canyon.Rock.002.mesh' }}
+      physical={{ raycast: true }}
+      done={ m => {m.addPhysicalShape({shape: 'mesh', mesh: 'Canyon.Rock.002_phy.obj', mass: 1 })  } }>
+    </mesh>
+  </node>
+  <node
+    position={{ x: 1151.9931640625, y: -1.6628880500793457, z: -1957.1591796875 }}
+    orientation={{ w: 0.7400898337364197, x: 0.0, y: 0.6725080013275146, z: -0.0 }}
+    scale={{ x: 0.9999999403953552, y: 0.9999999403953552, z: 1.0 }}>
+    <mesh
+      {...{ url: 'Canyon.Tree.001.mesh' }}
+      physical={{ raycast: true }}
+      done={ m => {m.addPhysicalShape({shape: 'mesh', mesh: 'Canyon.Tree.001_phy.obj', mass: 1 })  } }>
+    </mesh>
+  </node>
 </root>
