6d6e097035be388e3229d342aa4fb76764f1f80c
Daniel Meszaros <easimer@gmail.com>
2021-04-28 18:09:02 +0200

    compiler: add optimizations

--- devenv/compiler.js
+++ devenv/compiler.js
@@ -4,7 +4,7 @@ function getCommandKind(node) {
 
 function getCommandArgument(node) {
   let elemArgument = node.querySelector('.argument');
-  if(elemArgument != null) {
+  if(elemArgument !== null) {
     return elemArgument.value;
   } else {
     return null;
@@ -117,6 +117,127 @@ function traverseHtmlTree(context, tree, nodeConverter) {
   return ret;
 }
 
+function optimizeRemoveEmptyRepeats(program) {
+  const nodes = [program];
+  
+  let totalOrigCommands = 0;
+  let totalNewCommands = 0;
+
+  while(nodes.length > 0) {
+    const node = nodes.pop();
+
+    if(node.id === null) {
+      continue;
+    }
+
+    totalOrigCommands += node.children.length;
+
+    nodes.push(...node.children);
+
+    const nodesToMarkForDelete = node.children
+      .map((node, idx) => [idx, node])
+      .filter(kv => kv[1].id === 'REPEAT')
+      .filter(kv => kv[1].children.length === 0 || kv[1].arg === '0')
+      .map(kv => kv[1]);
+
+    for(const node of nodesToMarkForDelete) {
+      node.id = null;
+    }
+
+    node.children = node.children.filter(node => node.id !== null);
+    totalNewCommands += node.children.length;
+  }
+
+  return { name: 'Empty REPEATs', from: totalOrigCommands, to: totalNewCommands };
+}
+
+function optimizeRemoveNoops(program) {
+  const nodes = [program];
+
+  let totalOrigCommands = 0;
+  let totalNewCommands = 0;
+
+  while(nodes.length > 0) {
+    const node = nodes.pop();
+
+    for(const child of node.children) {
+      nodes.push(child);
+    }
+    
+    // If the current node has grandchildren then we can't perform this optimization
+    // (it's not impossible, it's just harder)
+    const singleLevel = node.children.reduce((prev, cur) => prev && (cur.children.length === 0), true);
+
+    if(!singleLevel) {
+      console.debug(`${node.id} is not single-level!`);
+      continue;
+    }
+
+    const numPushCmd = node.children.filter(op => op.id === 'STATE_PUSH').length;
+    const numPopCmd = node.children.filter(op => op.id === 'STATE_POP').length;
+    if(numPushCmd !== numPopCmd) {
+      console.debug(`${node} has mismatching PUSH/POP ops!`);
+      continue;
+    }
+    
+    totalOrigCommands += node.children.length;
+
+    const rangesToRemove = [];
+    const pushIndices = [];
+    let noopArray = [];
+    for(let i = 0; i < node.children.length; i++) {
+      const op = node.children[i];
+
+      if(op.id === 'STATE_PUSH') {
+        pushIndices.push(i);
+        noopArray.push(true);
+      } else if(op.id === 'STATE_POP') {
+        const matchingPush = pushIndices.pop();
+        const wasItNoop = noopArray.pop();
+        if(wasItNoop) {
+          rangesToRemove.push([matchingPush, i]);
+        }
+      } else if(op.id === 'MOVE_FORWARD' || op.id === 'MOVE_BACKWARD') {
+        // Drawing ops taint the entire noopArray
+        noopArray = noopArray.map(() => false);
+      }
+    }
+
+    console.assert(pushIndices.length === 0);
+
+    if(rangesToRemove.length === 0) {
+      continue;
+    }
+
+    for(const range of rangesToRemove) {
+      for(let i = range[0]; i <= range[1]; i++) {
+        // Mark for deletion
+        node.children[i].id = null;
+      }
+    }
+
+    node.children = node.children.filter(op => op.id !== null);
+
+    totalNewCommands += node.children.length;
+  }
+
+  return { name: 'PUSH:POP noop range', from: totalOrigCommands, to: totalNewCommands };
+}
+
+function mapOptimizationEfficiencyToColor(e) {
+  if(e < 10) {
+    return '#FF0000';
+  } else if(e < 20) {
+    return '#CC0000';
+  } else if(e < 40) {
+    return '#AA0000';
+  } else if(e < 80) {
+    return '#660000';
+  } else {
+    return '#440000';
+  }
+}
+
 export function makeProgramAST(treeRoot, strictMode) {
   let ret = parserNodeConverter.makeEmpty();
   const context = {
@@ -129,6 +250,21 @@ export function makeProgramAST(treeRoot, strictMode) {
     recursionCounter: 0,
   };
   ret = processSubcommands(context, ret, treeRoot, parserNodeConverter);
+  
+  const optimizationResults = [];
+  optimizationResults.push(optimizeRemoveNoops(ret));
+  optimizationResults.push(optimizeRemoveEmptyRepeats(ret));
+
+  // Print optimizations performed
+
+  console.group('Optimizations performed:');
+  for(const opt of optimizationResults) {
+    const percentageReduction = opt.to / opt.from * 100;
+    const color = mapOptimizationEfficiencyToColor(percentageReduction);
+
+    console.log(`%c${opt.name} - ${opt.from} -> ${opt.to} (${percentageReduction}%)`, `color: ${color}`);
+  }
+  console.groupEnd();
 
   if(context.numWarnings > 0 || context.numErrors > 0) {
     console.warn(`Compilation resulted in ${context.numWarnings} warnings and ${context.numErrors} errors!`);
@@ -155,7 +291,7 @@ export function dumpAST(ast, level) {
     ret += '[\n';
     for(let i = 0; i < ast.children.length; i++) {
       ret += dumpAST(ast.children[i], level + 1);
-      if(i != ast.children.length - 1) {
+      if(i !== ast.children.length - 1) {
         ret += ',';
       }
       ret += '\n';
